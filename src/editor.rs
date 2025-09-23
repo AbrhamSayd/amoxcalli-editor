38c6a67a0479840daa9cc61e57c8a4371399f1ac
Abrham Martinez <abrham.sayd@proton.me>
2025-09-23 13:14:04 -0700

    Initialize project with basic editor functionality

---
+++ src/editor.rs
@@ -0,0 +1,35 @@
+use crossterm::event::read;
+use crossterm::event::{Event::Key, KeyCode::Char, };
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+pub struct Editor {
+
+
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor{}
+    }
+    pub fn run(&self) -> Result<(), std::io::Error>{
+
+
+    enable_raw_mode().unwrap();
+    loop {
+        match read() {
+            Ok(Key(event)) => {
+                println!("{event:?} \r");
+                if let Char(c) = event.code {
+                    if c == 'q' {
+                        break;
+                    }
+                }
+            },
+            Err(err) => println!("Error: {err}"),
+        _=> ()
+        }
+    }
+    disable_raw_mode()
+}
+
+}

2917fde51642be24dafa5c2f02b02e521ba19513
Abrham Martinez <abrham.sayd@proton.me>
2025-09-25 14:37:21 -0700

    Refactor: improve terminal module organization and enhance error handling in editor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-#![warn(clippy::all, clippy::pedantic)]
-
-
+#![warn(clippy::all, clippy::pedantic, clippy::print_stdout)]
 mod editor;
 use editor::Editor;
+
 fn main() {
     Editor::default().run();
-    
 }

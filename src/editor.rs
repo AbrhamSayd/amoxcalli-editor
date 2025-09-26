b71901391858c3a6df57e15a68ed87cc974919c1
Abrham Martinez <asayd.mtz@proton.me>
2025-09-26 09:33:18 -0700

    Refactor: enhance event handling and improve caret movement in the editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,20 +1,30 @@
-use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use core::cmp::min;
+use crossterm::event::{
+    Event::{self, Key},
+    KeyCode::{self},
+    KeyEvent, KeyEventKind, KeyModifiers, read,
+};
 use std::io::Error;
 mod terminal;
-use terminal::{Terminal, Size, Position};
+use terminal::{Position, Size, Terminal};
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Copy, Clone, Default)]
 
+struct Location {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Default)]
 pub struct Editor {
     should_quit: bool,
+    location: Location,
 }
 
 impl Editor {
-    pub const fn default() -> Self {
-        Self { should_quit: false }
-    }
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
         let result = self.repl();
@@ -29,82 +39,129 @@ impl Editor {
                 break;
             }
             let event = read()?;
-            self.evaluate_event(&event);
+            self.evaluate_event(&event)?;
         }
         Ok(())
     }
-    fn evaluate_event(&mut self, event: &Event) {
+
+    fn move_point(&mut self, key_code: KeyCode) -> Result<(), Error> {
+        let Location { mut x, mut y } = self.location;
+        let Size { width, height } = Terminal::size()?;
+        match key_code {
+            KeyCode::Up => {
+                y = y.saturating_sub(1);
+            }
+            KeyCode::Down => {
+                y = min(height.saturating_sub(1), y.saturating_add(1));
+            }
+            KeyCode::Left => {
+                x = x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                x = min(width.saturating_sub(1), x.saturating_add(1));
+            }
+            KeyCode::PageUp => {
+                y = 0;
+            }
+            KeyCode::PageDown => {
+                y = height.saturating_sub(1);
+            }
+            KeyCode::Home => {
+                x = 0;
+            }
+            KeyCode::End => {
+                x = width.saturating_sub(1);
+            }
+            _ => (),
+        }
+        self.location = Location { x, y };
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) -> Result<(), Error> {
         if let Key(KeyEvent {
-            code, modifiers, ..
+            code,
+            modifiers,
+            kind: KeyEventKind::Press,
+            ..
         }) = event
         {
             match code {
-                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
                     self.should_quit = true;
+                }
+                KeyCode::Up
+                | KeyCode::Down
+                | KeyCode::Left
+                | KeyCode::Right
+                | KeyCode::PageUp
+                | KeyCode::PageDown
+                | KeyCode::End
+                | KeyCode::Home => {
+                    self.move_point(*code)?;
                 }
                 _ => (),
             }
         }
+        Ok(())
     }
     fn refresh_screen(&self) -> Result<(), Error> {
-        Terminal::hide_cursor()?;
+        Terminal::hide_caret()?;
+        Terminal::move_caret_to(Position::default())?;
         if self.should_quit {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n")?;
         } else {
-            //initialize screen
             Self::draw_rows()?;
-            Self::draw_welcome_message()?;
-            Terminal::move_cursor_to(Position { x: 0, y: 0})?;
+            Terminal::move_caret_to(Position {
+                col: self.location.x,
+                row: self.location.y,
+            })?;
         }
-        Terminal::show_cursor()?;
+        Terminal::show_caret()?;
         Terminal::execute()?;
         Ok(())
     }
-    fn draw_welcome_message() -> Result<(), Error>{
-        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
-        let width = Terminal::size()?.width as usize;
-        let len = welcome_message.len();
-        let padding = (width - len) / 2;
-        //TODO: WRAP LINES IF EXCEED WIDTH
-        if welcome_message.len() > width as usize {
-            welcome_message.truncate(width);
-        }
-        welcome_message = format!("~{}{}", " ".repeat(padding-1), welcome_message);
-        Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
-        Terminal::print(welcome_message.as_str())?;
-        Ok(())
-    }
-
-
     fn draw_rows() -> Result<(), Error> {
-        let Size{height, ..} = Terminal::size()?;
+        let Size { height, .. } = Terminal::size()?;
+        let welcome_row = height.saturating_sub(2) / 2; // Reserve space for help message
+        let help_row = welcome_row + 1;
+
         for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if current_row + 1 < height {
+
+            match current_row {
+                row if row == welcome_row => {
+                    let welcome_message = format!("{NAME} editor -- version {VERSION}");
+                    Self::draw_str(&welcome_message, current_row)?;
+                }
+                row if row == help_row => {
+                    // Display Help Message
+                    let help_message = "Ctrl-Q to quit";
+                    Self::draw_str(help_message, current_row)?;
+                }
+                _ => Terminal::print("~")?,
+            }
+
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
         Ok(())
     }
-    
-    fn draw_message(message: &str) -> Result<(), Error> {
-        let Size{width, height} = Terminal::size()?;
+
+    fn draw_str(message: &str, row: usize) -> Result<(), Error> {
+        let width = Terminal::size()?.width;
         let mut message = message.to_string();
-        if message.len() > width as usize {
-            message.truncate(width as usize);
+        let len = message.len();
+        let padding = if len >= width { 0 } else { (width - len) / 2 };
+        // TODO: WRAP LINES IF EXCEED WIDTH
+        if message.len() > width {
+            message.truncate(width);
         }
-
-        let x_middle_point = width as usize / 2 - message.len() / 2;
-        let y_middle_point = height as usize / 2;
-        message = format!("~{}{}", " ".repeat(x_middle_point), message);
-
-        Terminal::move_cursor_to(Position { x: 0, y: y_middle_point as u16 })?;
-        Terminal::print("~")?;
-
-        Terminal::move_cursor_to(Position { x: 0, y: y_middle_point as u16 })?;
-        Terminal::print(message.as_str())?;
+        let formatted_message = format!("~{}{}", " ".repeat(padding.saturating_sub(1)), message);
+        Terminal::move_caret_to(Position { col: 0, row })?;
+        Terminal::print(formatted_message.as_str())?;
         Ok(())
     }
 }
